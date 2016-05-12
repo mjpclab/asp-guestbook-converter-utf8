60120a235413d6747e3a2892d1eb2b41fd00fe7a
marjune <marjune@163.com>
2016-05-12 22:49:22 +0800

    improve performance of replace charset declare

--- shenj-utf8.sh
+++ shenj-utf8.sh
@@ -12,14 +12,19 @@ echo -n 'Converting charset to UTF-8...'
 find "$basedir" -type f \( -iname '*.asp' -o -iname '*.inc' \) -print0 | while read -d $'\0' file; do
 	iconv -s -f GBK -t UTF-8 "$file" > "$file".tmp
 	\mv -f "$file".tmp "$file"
-
 done;
 [ $? -eq 0 ] && echo ' [OK]' || echo ' [Failed!]'
 
 echo -n 'Replacing charset declaration...'
-find "$basedir" -type f \( -iname '*.asp' -o -iname '*.inc' \) -print0 | while read -d $'\0' file; do
+find "$basedir" -type f \( -iname '*.asp' -o -iname '*.inc' \) -print0 | xargs -0 grep -Zl '[Cc]harset' 2>/dev/null | while read -d $'\0' file; do
 	sed -b -e '/[Cc]harset/s/gbk/utf-8/' "$file" > "$file".tmp
-	sed -b -e '/[Cc]ode[Pp]age/s/936/65001/' "$file".tmp > "$file"
-	\rm -f "$file".tmp
+	\mv -f "$file".tmp "$file"
+done;
+[ $? -eq 0 ] && echo ' [OK]' || echo ' [Failed!]'
+
+echo -n 'Replacing codepage declaration...'
+find "$basedir" -type f \( -iname '*.asp' -o -iname '*.inc' \) -print0 | xargs -0 grep -Zl '[Cc]ode[Pp]age' 2>/dev/null | while read -d $'\0' file; do
+	sed -b -e '/[Cc]ode[Pp]age/s/936/65001/' "$file" > "$file".tmp
+	\mv -f "$file".tmp "$file"
 done;
 [ $? -eq 0 ] && echo ' [OK]' || echo ' [Failed!]'
